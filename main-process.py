a5a02445a6ad091462f4965f60039084921472b4
aviiciii <laavesh1@gmail.com>
2023-07-07 22:20:10 +0530

    sort by desc

--- main-process.py
+++ main-process.py
@@ -11,18 +11,22 @@ def count_word_frequency(text):
     return word_counts
 
 def save_word_frequency_to_csv(word_counts, filename):
-    
+    # Sort the word-frequency pairs by frequency in descending order
+    sorted_word_counts = sorted(word_counts.items(), key=lambda x: x[1], reverse=True)
+
+    # Write the word-frequency pairs to a CSV file
     with open(filename, 'w', newline='') as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow(['Word', 'Frequency'])
         writer.writerows(sorted_word_counts)
 
-# Example usage
+# Read the text from the cleaned input file
 with open('input/cleaned_input.txt', 'r') as f:
     text = f.read()
 
+# Count the frequency of each word
 word_counts = count_word_frequency(text)
 
-
+# Save the word-frequency pairs to a CSV file
 save_word_frequency_to_csv(word_counts, 'word_frequency.csv')
-print("Word frequency CSV file has been created.")
+print("Done!")
