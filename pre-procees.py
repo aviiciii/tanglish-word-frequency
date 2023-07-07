1e0d66f99e0b11b1c12cfa4b29ca5dd5f44eaef4
aviiciii <laavesh1@gmail.com>
2023-07-07 22:02:51 +0530

    optimised pre-processing

--- pre-procees.py
+++ pre-procees.py
@@ -5,67 +5,34 @@ import string
 with open('input/combined_input.txt', 'r') as f:
     raw_text = f.read()
 words = raw_text.split(' ')
-print('Total words: ', len(words))
-total_words = len(words)
+print('Total words:', len(words))
 
-
-def remove_emojis_from_list(text_list):
+def remove_noise_from_list(text_list):
+    noise_set = set(['', ' ', '\n'])  # Set of noise elements to remove
     emoji_pattern = re.compile("[^\x00-\x7F]+")
-    text_list_without_emojis = []
+    punctuation_table = str.maketrans("", "", string.punctuation)
 
-    for text in text_list:
-        text_without_emojis = emoji_pattern.sub('', text)
-        text_list_without_emojis.append(text_without_emojis)
+    text_list_size = len(text_list)
+    i = 0
+    while i < text_list_size:
+        # Remove emojis
+        text_list[i] = emoji_pattern.sub('', text_list[i])
+        # Remove punctuation
+        text_list[i] = text_list[i].translate(punctuation_table)
+        # Remove numbers
+        text_list[i] = re.sub(r'\d+', '', text_list[i])
 
-    return text_list_without_emojis
+        if text_list[i] in noise_set or len(text_list[i]) <= 1:
+            # Remove empty strings and single characters
+            text_list.pop(i)
+            text_list_size -= 1
+        else:
+            i += 1
 
+    return text_list
 
+# Remove noise from the list of words
+words = remove_noise_from_list(words)
+after_noise_removal = len(words)
 
-def remove_punctuations_from_list(text_list):
-    text_list_without_punctuations = []
-
-    for text in text_list:
-        # Remove punctuations using the string.punctuation constant
-        text_without_punctuations = text.translate(str.maketrans("", "", string.punctuation))
-        text_list_without_punctuations.append(text_without_punctuations)
-
-    return text_list_without_punctuations
-
-def remove_numbers_from_list(text_list):
-    text_list_without_numbers = []
-
-    for text in text_list:
-        # Remove numbers using regular expressions
-        text_without_numbers = re.sub(r'\d+', '', text)
-        text_list_without_numbers.append(text_without_numbers)
-
-    return text_list_without_numbers
-
-
-# remove the empty string
-words = [word for word in words if word != '']
-after_remove_empty = len(words)
-print('Removed empty: ', total_words - after_remove_empty)
-
-# remove emojis
-words = remove_emojis_from_list(words)
-after_remove_emojis = len(words)
-print('Removed emojis: ', after_remove_empty - after_remove_emojis)
-
-# remove punctuations
-words = remove_punctuations_from_list(words)
-after_remove_punctuations = len(words)
-print('Removed punctuations: ', after_remove_emojis - after_remove_punctuations)
-
-
-# remove numbers
-words = remove_numbers_from_list(words)
-after_remove_numbers = len(words)
-print('Removed numbers: ', after_remove_punctuations - after_remove_numbers)
-
-
-# remove single characters
-words = [word for word in words if len(word) > 1]
-after_remove_single_characters = len(words)
-
-
+print('Words after noise removal:', after_noise_removal)
